4428356546be00cd2fe79382275c47805eab0c1d
SAMRIDDHI-1 <114829223+SAMRIDDHI-1@users.noreply.github.com>
2024-12-27 00:01:42 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
@@ -5,6 +17,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  // Add exportPathMap for static export
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      // Add more routes as necessary
+    };
+  },
 };
 
 module.exports = nextConfig;
